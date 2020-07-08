b31c20bd778a348b081d5304fe0d9ba2eb841f6a
jyothi14691 <jyothi14691@gmail.com>
2020-07-08 18:17:10 -0400

    Lab Compiled with changes

--- TicketMachine.java
+++ TicketMachine.java
@@ -9,7 +9,7 @@
  * @author David J. Barnes and Michael Kolling
  * @version 2008.03.30
  */
-public class TicketMachine
+ class TicketMachine
 {
     // The price of a ticket from this machine.
     private Integer price;
@@ -94,5 +94,13 @@ public class TicketMachine
         balance = 0;
         
         return "Ticket price: " + price + " cents. " + "Your total is " + total + ".";
+    }
+    
+    public class Student
+    {
+        int status;
+    }
+    public class LabClass
+    {
     }
 }
